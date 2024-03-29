034017c7557eaa29c10b715e28f8e9e03bea722b
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2024-03-29 16:51:11 +0530

    Fix formatting in ImageDetails component

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -9,6 +9,7 @@ import { getImageById } from "@/lib/actions/image.actions";
 import { getImageSize } from "@/lib/utils";
 import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
 
+
 const ImageDetails = async ({ params: { id } }: SearchParamProps) => {
   const { userId } = auth();
 
